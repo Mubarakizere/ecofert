84a16ea322dd3c46e09f90d999522aca44753c0c
Izere Moubarak <izeremubarak05@gmail.com>
2026-09-04 12:34:22 +0200

    Remove DemoDataSeeder for clean dashboard with zero data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             ApprovedFormulationSeeder::class,
-            DemoDataSeeder::class,
         ]);
     }
 }
